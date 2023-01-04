93bd892bb768a7e9cd23ba7974a41274bf3aa4f0
porterlu <1258210724@qq.com>
2023-01-04 23:14:40 +0800

    review 1 time

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -29,8 +29,8 @@ impl ParsePosNonzeroError {
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
+    // map if ok return ok(func(x)) else err(e)
+    // map_err if ok return ok(x) else err(func(x))
     let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
